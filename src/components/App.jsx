d6e6414a3dd4eaa352a3c2d8af14c1d009fec637
Joacoetruuu <joacoetruuu@gmail.com>
2023-01-27 02:57:06 -0300

    hola

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,23 @@
 import { useEffect, useState } from "react"
-import { getP2p } from "../data/getData";
 import { DivP2P } from "./DivP2P";
+import { Header } from "./Header";
 
 export function App() {
 
+  
+
+  const [criptoUse, setCriptoUse] = useState("btc");
+  const [methodType, setMethodType] = useState("sell"); 
+
   const [data, setData] = useState({});
+
+  async function getP2p(){
+    let res = await fetch(`https://criptoya.com/api/binancep2p/${methodType}/${criptoUse}/ars/20`)
+    .catch(error => console.log(error))
+    let data = await res.json(); 
+
+    return data 
+}
 
   useEffect(() => {
     async function get(){
@@ -12,10 +25,13 @@ export function App() {
     }; 
     get(); 
   }, [])
+
+
   
 
   return(
     <>
+        <Header setMethodType={setMethodType} methodType={methodType} criptoUse={criptoUse} setCriptoUse={setCriptoUse} getP2p={getP2p} setData={setData}/>
         <div className="container">
           <DivP2P data={data}></DivP2P>
         </div>
--- src/data/getData.js
+++ src/data/getData.js
@@ -1,6 +1,2 @@
-export async function getP2p(){
-    let res = await fetch("https://criptoya.com/api/binancep2p/sell/dai/ars/20")
-    let data = await res.json(); 
 
-    return data 
-}
+
